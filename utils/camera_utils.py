30e21e01ff428d69dcf4cb240488667216f2dd94
Payday02 <padendavis@gmail.com>
2026-08-07 18:44:57 -0500

    Fix unbounded memory growth in image loading (see previous commit message for details)

--- utils/camera_utils.py
+++ utils/camera_utils.py
@@ -11,13 +11,14 @@
 
 from scene.cameras import Camera
 import numpy as np
+from PIL import Image
 from utils.general_utils import PILtoTorch
 from utils.graphics_utils import fov2focal
 
 WARNED = False
 
 def loadCam(args, id, cam_info, resolution_scale):
-    orig_w, orig_h = cam_info.image.size
+    orig_w, orig_h = cam_info.width, cam_info.height
 
     if args.resolution in [1, 2, 4, 8]:
         resolution = round(orig_w/(resolution_scale * args.resolution)), round(orig_h/(resolution_scale * args.resolution))
@@ -38,7 +39,9 @@ def loadCam(args, id, cam_info, resolution_scale):
         scale = float(global_down) * float(resolution_scale)
         resolution = (int(orig_w / scale), int(orig_h / scale))
 
-    resized_image_rgb = PILtoTorch(cam_info.image, resolution)
+    image = Image.open(cam_info.image_path)
+    resized_image_rgb = PILtoTorch(image, resolution)
+    image.close()
 
     gt_image = resized_image_rgb[:3, ...]
     loaded_mask = None
